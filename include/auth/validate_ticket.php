afc4b1de406541c84f82ce378f040f2317683cda
Michael Dietzel <>
2019-05-15 22:55:11 +0200

    completed implementation of login with sessions.

--- include/auth/validate_ticket.php
+++ include/auth/validate_ticket.php
@@ -1,48 +1,44 @@
 <?php
-	session_start();
-	include($_SERVER["DOCUMENT_ROOT"] . "/Project_Ziphon/include/init/constant.php");
-	include(INC . "connect.php");
-	include(INC . "function.php");
-	include(CL . "message_class.php");
 
+/**
+ * Checks, whether the ticket_id given via post exists in the database. If it does $_SESSION["player_id"] is set accordingly
+ * 
+ * @return returns an array consisting of a bool for success and a message to display
+ */
+function validate_ticket(mysqli $con)
+{
 	$message = new message();
-	$ip = IP;
-	if(empty($_REQUEST["id"]))
+	if(!isset($_POST["ticket_id"]))
+	{
+		return array(false, $message);
+	}	
+	$ticket_id = trim($_POST["ticket_id"]);
+	if($ticket_id == "")
 	{
 		$message->getMessageCode("ERR_TICKET_MISSING");
-		echo json_encode(array("message" => $message->displayMessage(), "step" => "0"));
-	} else {
-		$ticket_hash = sha1($_REQUEST["id"]); //TODO: maybe replace sha1 by something better, for example password_hash
-		$result = mysqli_query($con, "SELECT ID, name FROM player WHERE ticket_id = '$ticket_hash';"); //todo: rename colum ticket_id to ticket_hash
-		
-		$err = mysqli_error($con);
-		
-		if(mysqli_num_rows($result) == 0)
-		{
-			$message->getMessageCode("ERR_VAL_FAILED");
-			echo json_encode(array("message" => $message->displayMessage(), "step" => "0"));
-			//TODO: better error message?: ticket id not found / wrong ticket id
-		}
-		elseif(mysqli_num_rows($result) > 1)
-		{
-			$message->getMessageCode("ERR_VAL_FAILED");
-			echo json_encode(array("message" => $message->displayMessage(), "step" => "0"));
-			//TODO: better error message?: multiple players with identical ticket id
-		}
-		else
-		{
-			$row = mysqli_fetch_assoc($result);
-			$_SESSION["player_id"] = $row["ID"];
-			
-			if($row["name"] == "")
-			{
-				//TODO: player needs to select a name
-			}
-			else
-			{
-				$message->getMessageCode("SUC_VAL_APPROVED");
-				echo json_encode(array("message" => $message->displayMessage(), "step" => "2"));
-			}
-		}
+		return array(false, $message);
 	}
-?>
+
+	$ticket_hash = sha1($ticket_id); //TODO: maybe replace sha1 by something better, for example password_hash
+	$result = mysqli_query($con, "SELECT ID FROM player WHERE ticket_id = '$ticket_hash';"); //todo: rename colum ticket_id to ticket_hash
+
+	if(mysqli_num_rows($result) == 0)
+	{
+		$message->getMessageCode("ERR_VAL_FAILED");
+		//TODO: better error message?: ticket id not found / wrong ticket id
+		return array(false, $message);
+	}
+	elseif(mysqli_num_rows($result) > 1)
+	{
+		$message->getMessageCode("ERR_VAL_FAILED");
+		//TODO: better error message?: multiple players with identical ticket id
+		return array(false, $message);
+	}
+	else
+	{
+		$row = mysqli_fetch_assoc($result);
+		$_SESSION["player_id"] = (int)$row["ID"];
+		$message->getMessageCode("SUC_VAL_APPROVED");
+		return array(true, $message);
+	}
+}
