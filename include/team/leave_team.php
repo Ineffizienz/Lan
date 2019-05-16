4672c7245fdc8f6db46e1e532289d04ad91fd47e
Ineffizienz <35340645+Ineffizienz@users.noreply.github.com>
2019-05-16 12:58:13 +0200

    Implementing session
    - removed not required user_id as we added the session to every part of the code

--- include/team/leave_team.php
+++ include/team/leave_team.php
@@ -8,13 +8,12 @@
 
 	$player_id = $_SESSION["player_id"];
 	$team_id = $_REQUEST["team"];
-	$user = $_REQUEST["user"]; // ?
 	$message = new message();
 	$achievement = new Progress();
 
 	if(!empty($team_id))
 	{
-		$sql = "UPDATE player SET team_id = NULL WHERE ID = '$user'";
+		$sql = "UPDATE player SET team_id = NULL WHERE ID = '$player_id'";
 
 		if (mysqli_query($con,$sql) === TRUE)
 		{
