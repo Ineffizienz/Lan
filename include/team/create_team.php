ba82ffadfa7c2097096632f5a9e245f654080f35
Ineffizienz <35340645+Ineffizienz@users.noreply.github.com>
2019-07-03 18:28:04 +0200

    Fix file handling js

--- include/team/create_team.php
+++ include/team/create_team.php
@@ -13,35 +13,35 @@ include(INC . "function.php");
 
 	if(isset($_REQUEST["team"]))
 	{
-				$t_name = $_REQUEST["team"];
+		$t_name = $_REQUEST["team"];
 
-				if ($t_name == "")
-				{
-					$message->getMessageCode("ERR_NO_TEAM_NAME");
-					$achievement->getTrigger($con,$player_id,"Sir Brummel");
-					echo json_encode(array("message" => $message->displayMessage(),"achievement" => $achievement->showAchievement()));
+		if ($t_name == "")
+		{
+			$message->getMessageCode("ERR_NO_TEAM_NAME");
+			$achievement->getTrigger($con,$player_id,"Sir Brummel");
+			echo json_encode(array("message" => $message->displayMessage(),"achievement" => $achievement->showAchievement()));
 
-				} else {
-					if(strstr($t_name,"\\"))
-					{
-						$t_name = addcslashes($t_name, "\\");
-					}
-					$teams = getTeamNames($con);
+		} else {
+			if(strstr($t_name,"\\"))
+			{
+				$t_name = addcslashes($t_name, "\\");
+			}
+			$teams = getTeamNames($con);
 
-					if (!empty($teams) && in_array($t_name,$teams))
-					{
-						
-						$message->getMessageCode("ERR_TEAM_NAME_EXISTS");
-						$achievement->getTrigger($con,$player_id,"Sir Brummel");
-						echo json_encode(array("message" => $message->displayMessage(),"achievement" => $achievement->showAchievement()));
+			if (!empty($teams) && in_array($t_name,$teams))
+			{
+				
+				$message->getMessageCode("ERR_TEAM_NAME_EXISTS");
+				$achievement->getTrigger($con,$player_id,"Sir Brummel");
+				echo json_encode(array("message" => $message->displayMessage(),"achievement" => $achievement->showAchievement()));
 
-					} else {
-						mysqli_query($con,"INSERT INTO tm_teamname (name,color) VALUES ('$t_name',NULL)");
+			} else {
+				mysqli_query($con,"INSERT INTO tm_teamname (name,color) VALUES ('$t_name',NULL)");
 
-						$message->getMessageCode("SUC_CREATE_TEAM");
-						echo json_encode(array("message" => $message->displayMessage()));
+				$message->getMessageCode("SUC_CREATE_TEAM");
+				echo json_encode(array("message" => $message->displayMessage()));
 
-					}
-				}
+			}
+		}
 	}
 ?>
