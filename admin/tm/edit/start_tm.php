8b069a20e761d38018341054d11cce214335a7bf
Ineffizienz <35340645+Ineffizienz@users.noreply.github.com>
2019-12-06 14:08:23 +0100

    Fixed match creation
    + fixed an issue where two matches where created for one matchup

--- admin/tm/edit/start_tm.php
+++ admin/tm/edit/start_tm.php
@@ -43,8 +43,6 @@
 
                         for ($i=1;$i<=$rounded_count;$i++)
                         {
-                            $last_pair_id = "";
-                            $first_pair_id = "";
                             $next_stage = $stage_count + 1;
                             $sql = "INSERT INTO tm_paarung (team_1, team_2, tournament, stage) VALUES (NULL, NULL, '$tm_id', '$next_stage')";
                             if(mysqli_query($con,$sql))
@@ -52,21 +50,31 @@
                                 $last_pair_id = getLastPairId($con,$tm_id);
                                 $first_pair_id = getFirstPairId($con,$tm_id,$stage_count);
                                 
+                                $match_count = 1;
                                 foreach ($first_pair_id as $pair_id)
                                 {
-                                    $sql = "INSERT INTO tm_match (result_team1, result_team2) VALUES (NULL, NULL)";
-                                    if (mysqli_query($con,$sql))
+                                    if($match_count > 1)
                                     {
-                                        $new_match_id = getNewMatchId($con);
-                                        $sql = "INSERT INTO tm_matches (match_id) VALUES ('$new_match_id')";
-                                        if(mysqli_query($con,$sql))
+                                        $sql = "UPDATE tm_paarung SET successor = '$last_pair_id' WHERE ID = '$pair_id'";
+                                        mysqli_query($con,$sql);
+                                    } else {
+                                        $sql = "INSERT INTO tm_match (result_team1, result_team2) VALUES (NULL, NULL)";
+                                        if (mysqli_query($con,$sql))
                                         {
-                                            $new_matches_id = getNewMatchesId($con);
-                                            $sql = "UPDATE tm_paarung SET matches_id = '$new_matches_id' WHERE (ID = '$last_pair_id') AND (successor IS NULL)";
+                                            $new_match_id = getNewMatchId($con);
+                                            $sql = "INSERT INTO tm_matches (match_id) VALUES ('$new_match_id')";
                                             if(mysqli_query($con,$sql))
                                             {
-                                                $sql = "UPDATE tm_paarung SET successor = '$last_pair_id' WHERE ID = '$pair_id'";
-                                                mysqli_query($con,$sql);
+                                                $new_matches_id = getNewMatchesId($con);
+                                                $sql = "UPDATE tm_paarung SET matches_id = '$new_matches_id' WHERE (ID = '$last_pair_id') AND (successor IS NULL)";
+                                                if(mysqli_query($con,$sql))
+                                                {
+                                                    $sql = "UPDATE tm_paarung SET successor = '$last_pair_id' WHERE ID = '$pair_id'";
+                                                    mysqli_query($con,$sql);
+                                                } else {
+                                                    $message->getMessageCode("ERR_ADMIN_DB");
+                                                    echo buildJSONOutput($message->displayMessage() . mysqli_error($con));
+                                                }
                                             } else {
                                                 $message->getMessageCode("ERR_ADMIN_DB");
                                                 echo buildJSONOutput($message->displayMessage() . mysqli_error($con));
@@ -75,10 +83,8 @@
                                             $message->getMessageCode("ERR_ADMIN_DB");
                                             echo buildJSONOutput($message->displayMessage() . mysqli_error($con));
                                         }
-                                    } else {
-                                        $message->getMessageCode("ERR_ADMIN_DB");
-                                        echo buildJSONOutput($message->displayMessage() . mysqli_error($con));
                                     }
+                                    $match_count++;
                                 }
                             } else {
                                 $message->getMessageCode("ERR_ADMIN_DB");
