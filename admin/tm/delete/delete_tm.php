0c15a326e41d98faa8f7a289ff637c137caf2dd0
Ineffizienz <35340645+Ineffizienz@users.noreply.github.com>
2019-12-17 18:16:40 +0100

    extend delete tournament
    + extended the functionality of deleting tournaments so admins will be able to delete tournaments even if they are not started yet #176

--- admin/tm/delete/delete_tm.php
+++ admin/tm/delete/delete_tm.php
@@ -2,7 +2,6 @@
     include(dirname(__FILE__,4) . "/include/init/constant.php");
     include(dirname(__FILE__,3) . "/include/admin_func.php");
     include(CL . "message_class.php");
-    include(INIT . "get_parameters.php");
     include(INC . "connect.php");
 
     $tm_id = $_REQUEST["tm_id"];
@@ -15,48 +14,62 @@
         $message->getMessageCode("ERR_ADMIN_TM_DOES_NOT_EXISTS");
         echo buildJSONOutput($message->displayMessage());
     } else {
-        $tm_period_id = getPeriodIdFromTournament($con,$tm_id);
-        $sql = "DELETE FROM tm_period WHERE ID = '$tm_period_id";
-        if(mysqli_query($con,$sql))
+        $tm_status = getTournamentStatus($con,$tm_id);
+        if($tm_status == "1")
         {
-            $sql = "DELETE FROM tm_gamerslist WHERE tm_id = '$tm_id'";
+            $tm_period_id = getPeriodIdFromTournament($con,$tm_id);
+            $sql = "DELETE FROM tm_period WHERE ID = '$tm_period_id";
             if(mysqli_query($con,$sql))
             {
-                $matches_ids = getMatchesIdFromPaarung($con,$tm_id);
-                foreach ($matches_ids as $matches_id)
+                $sql = "DELETE FROM tm_gamerslist WHERE tm_id = '$tm_id'";
+                if(mysqli_query($con,$sql))
                 {
-                    $match_id = getMatchIdFromMatches($con,$matches_id);
-                    $sql = "DELETE FROM tm_match WHERE ID = '$match_id'";
-                    if(mysqli_query($con,$sql))
+                    $matches_ids = getMatchesIdFromPaarung($con,$tm_id);
+                    foreach ($matches_ids as $matches_id)
                     {
-                        $sql = "DELETE FROM tm_paarung WHERE tournament = '$tm_id' AND tm_matches_id = '$matches_id'";
-                        if(!mysqli_query($con,$sql))
+                        $match_id = getMatchIdFromMatches($con,$matches_id);
+                        $sql = "DELETE FROM tm_match WHERE ID = '$match_id'";
+                        if(mysqli_query($con,$sql))
                         {
+                            $sql = "DELETE FROM tm_paarung WHERE tournament = '$tm_id' AND tm_matches_id = '$matches_id'";
+                            if(!mysqli_query($con,$sql))
+                            {
+                                $message->getMessageCode("ERR_ADMIN_DB");
+                                echo buildJSONOutput(array($message->displayMessage() . mysqli_error($con)));
+                            }
+                        } else {
                             $message->getMessageCode("ERR_ADMIN_DB");
                             echo buildJSONOutput(array($message->displayMessage() . mysqli_error($con)));
                         }
-                    } else {
-                        $message->getMessageCode("ERR_ADMIN_DB");
-                        echo buildJSONOutput(array($message->displayMessage() . mysqli_error($con)));
                     }
-                }
-                $sql = "DELETE FROM tm WHERE ID = '$tm_id'";
-                if(mysqli_query($con,$sql))
-                {
-                    $message->getMessageCode("SUC_ADMIN_DELETE_TM");
-                    echo buildJSONOutput(array($message->displayMessage(),"#tm_maintain","#tm_list"));
+                    $sql = "DELETE FROM tm WHERE ID = '$tm_id'";
+                    if(mysqli_query($con,$sql))
+                    {
+                        $message->getMessageCode("SUC_ADMIN_DELETE_TM");
+                        echo buildJSONOutput(array($message->displayMessage(),"#tm_maintain","#tm_list"));
+                    } else {
+                        $message->getMessageCode("ERR_ADMIN_INTERN_#4");
+                        echo buildJSONOutput($message->displayMessage() . mysqli_error($con));
+                    }
+    
                 } else {
-                    $message->getMessageCode("ERR_ADMIN_INTERN_#4");
+                    $message->getMessageCode("ERR_ADMIN_DB");
                     echo buildJSONOutput($message->displayMessage() . mysqli_error($con));
                 }
- 
+            } else {
+                $message->getMessageCode("ERR_ADMIN_DB");
+                echo buildJSONoutput($message->displayMessage() . mysqli_error($con));
+            }
+        } else {
+            $sql = "DELETE FROM tm WHERE ID = '$tm_id'";
+            if(mysqli_query($con,$sql))
+            {
+                $message->getMessageCode("SUC_ADMIN_DELETE_TM");
+                echo buildJSONOutput(array($message->displayMessage(),"#tm_maintain","#tm_list"));
             } else {
                 $message->getMessageCode("ERR_ADMIN_DB");
                 echo buildJSONOutput($message->displayMessage() . mysqli_error($con));
             }
-        } else {
-            $message->getMessageCode("ERR_ADMIN_DB");
-            echo buildJSONoutput($message->displayMessage() . mysqli_error($con));
         }
     }
 ?>
