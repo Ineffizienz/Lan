1f04c7dbf8f8720c2fed3ccef95a84448f056372
Ineffizienz <35340645+Ineffizienz@users.noreply.github.com>
2020-06-09 14:21:21 +0200

    Moving match data to tm_paarung
    + moved match results to tm_paarung #231
    + added new way of updating the db regarding changes in structure

---
+++ update/update_1.6.1.php
@@ -0,0 +1,11 @@
+<?php
+include(dirname(__FILE__,2) . "/include/connect.php");
+include("update_file.php");
+
+addRealNameToPlayer($con);
+changePlayerCountDataType($con);
+addMatchResultToPaarung($con);
+transferMatchData($con);
+deleteTmMatches($con);
+deleteTmMatch($con);
+?>
