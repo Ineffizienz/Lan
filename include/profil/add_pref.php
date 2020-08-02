93b88549d5b3f41c4480170e466e2cc392c572ec
Ineffizienz <35340645+Ineffizienz@users.noreply.github.com>
2020-08-02 13:54:55 +0200

    Removed achievements from preference handling
    - removed achievements from preference handling

--- include/profil/add_pref.php
+++ include/profil/add_pref.php
@@ -4,17 +4,14 @@
     include(INC . "connect.php");
     include(INIT . "get_parameters.php");
     include(CL . "message_class.php");
-    include(CL . "progress_class.php");
     include(CL . "player_class.php");
 
     $message = new message();
-    $achievement = new Progress();
     $player = new Player($con, $_SESSION["player_id"]);
     $game_id = $_REQUEST["checkedGame"];
 
     if($_REQUEST["state"] == "checked")
     {
-        //INSERT new value
         $message->getMessageCode($player->setNewPreference($game_id));
         echo json_encode(array("message" => $message->displayMessage()));
     } else {
