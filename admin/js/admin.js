0603950632aba20985573381e01fcde4d362dcd4
Ineffizienz <35340645+Ineffizienz@users.noreply.github.com>
2020-08-12 16:28:33 +0200

    BFFV to game banner
    + added bffv to game banner handling

--- admin/js/admin.js
+++ admin/js/admin.js
@@ -206,14 +206,18 @@ $(document).ready(function(){
 
 		form_data.append("game_id",game_id);
 
-		if($(banner_id).get(0).files.length == 0)
+		if(!fileValidation(image))
 		{
-			form_data.append("file","0");
+			console.log("File error");
 		} else {
-			form_data.append("file",image);
+			if(image.length == 0)
+			{
+				form_data.append("file","0");
+			} else {
+				form_data.append("file",image);
+			}
+			postFileAjax(form_data,getEndpoint("update_game_banner"),showResult(banner_id));
 		}
-
-		postFileAjax(form_data,getEndpoint("update_game_banner"),showResult(banner_id));
 	}
 
 	function getFile(event) //Upload for new Keys
